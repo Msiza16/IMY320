863c8e7f7f96489940d3b7e79ee5de1e29f81cc6
PATIENCE MTSWENI <u11116774@tuks.co.za>
2015-09-22 19:18:45 +0200

    Added ToDoList.java File

---
+++ ToDoList.java
@@ -0,0 +1,35 @@
+import java.util.Date;
+
+/**
+ * @Patience Mtsweni 11116774
+ */
+public class ToDoList {
+
+    private Task task [];
+
+    public void createTask(){
+
+    }
+
+    public void removeTask(){
+
+    }
+
+    public void updateTask(){
+
+    }
+    
+    private class Task{
+
+        private String taskName;
+
+        private Date taskDate;
+
+        private String taskPriority;
+
+        private String taskStatus;
+
+        /** TODO: Methods **/
+
+    }
+}
