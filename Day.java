b9fa230f4cdb62ff6f01848d7410349fca4c0f80
Msiza <dunique4@gmail.com>
2015-09-22 19:03:04 +0200

    Calendar java files

---
+++ Day.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ElementCode
+ */
+public class Day 
+{
+    int day;
+    String dayOfWeek[]={"Monday","Tuesday","Wednesday","Thursday","Friday","Saterday","Sunday"};
+    private class time
+    {
+      public int hr;
+      public int min;
+      public int sec;
+      public void setTime(int ...arg)
+      {
+      
+      }
+      
+      @Override 
+      public String toString()
+      {
+      
+      return " ";
+      }
+      
+    }
+}
